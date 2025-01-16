0fa69aab70057420a0d6e81b3b89127f5034a9c2
Lucas Daniel <118680767+lucaasd@users.noreply.github.com>
2025-01-16 20:23:26 -0300

    refactor(vm/memory): change variables from Variable[] to List<Variable> for more flexibility

--- VM/Memory/Frame.cs
+++ VM/Memory/Frame.cs
@@ -3,12 +3,12 @@ using VM.Memory;
 public class Frame
 {
     private Stack<byte> stack = [];
-    private Variable[] variables = new Variable[16];
+    private List<Variable> variables = [];
     private Frame? parent;
     private string name = "";
 
     public Stack<byte> Stack { get => stack; set { stack = value; } }
-    public Variable[] Variables { get => variables; set => variables = value; }
+    public List<Variable> Variables { get => variables; set => variables = value; }
     public Frame? Parent => parent;
 
     public Frame(string name, Frame? parent = null)
