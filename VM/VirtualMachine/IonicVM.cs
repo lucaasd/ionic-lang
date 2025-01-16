1fbd3400a1b74f6b3303b061f103e51df38fdb0e
Lucas Daniel <118680767+lucaasd@users.noreply.github.com>
2025-01-16 17:05:18 -0300

    refactor(vm): reimplement PUSH instruction

--- VM/VirtualMachine/IonicVM.cs
+++ VM/VirtualMachine/IonicVM.cs
@@ -1,24 +1,20 @@
 using System.Text;
 using VM.Instructions;
-using VM.Extensions;
 using VM.Instructions.Declarations;
-using VM.Memory;
 using VM.AssemblyInfo.Validation;
-using VM.VirtualMachine.Runtime;
+using VM.Runtime;
+
 namespace VM.VirtualMachine;
 
 public class IonicVM
 {
-    public static bool IsLong(byte[] bytes) => bytes.Length == 8;
-    public static bool IsInteger(byte[] bytes) => bytes.Length == 4;
-    public static bool IsByte(byte[] bytes) => bytes.Length == 1;
-
     private List<IByteCodePart> code = [];
     private List<Function> functions = [];
     private Frame currentFrame;
     private readonly Frame mainFrame;
     private Function? currentFunction;
 
+    private Dictionary<Instruction, Action<byte[]>> opcodeActionDictionary;
     private int programCounter;
 
     public Stack<byte>? Stack => currentFrame?.Stack;
@@ -27,6 +23,10 @@ public class IonicVM
     {
         mainFrame = new Frame("_");
         currentFrame = mainFrame;
+        opcodeActionDictionary = new()
+        {
+            {Instruction.PUSH, ExecutePush}
+        };
     }
 
     public string ConvertCodeToHumanIL()
@@ -51,7 +51,6 @@ public class IonicVM
 
     private void Run(List<IByteCodePart> code)
     {
-        Console.WriteLine("Running code");
         while (programCounter < code.Count)
         {
             if (currentFunction is null)
@@ -61,24 +60,10 @@ public class IonicVM
                     var operation = (code[programCounter] as Operation)!;
 
                     var instruction = operation.Instruction;
-                    var operand = operation.Operand;
+                    var operand = operation?.Operand;
 
-                    if (IsByte(operand))
-                    {
-                        Execute(instruction, operand[0]);
-                    }
-                    else if (IsInteger(operand))
-                    {
-                        Execute(instruction, BitConverter.ToInt32(operand));
-                    }
-                    else if (IsLong(operand))
-                    {
-                        Execute(instruction, BitConverter.ToInt64(operand));
-                    }
-                    else
-                    {
-                        Execute(instruction, operand);
-                    }
+
+                    Execute(instruction, operand ?? []);
                 }
                 else if (code[programCounter] is FunctionDeclaration && currentFunction is null)
                 {
@@ -97,7 +82,6 @@ public class IonicVM
                         throw new InvalidOperationException($"Descriptor {descriptor} is not valid");
                     }
 
-                    Console.WriteLine($"Function {functionDeclaration.Name} declared");
                     programCounter++;
                 }
                 else if (code[programCounter] is Operation && currentFunction is not null)
@@ -138,134 +122,15 @@ public class IonicVM
 
     private void Execute(Instruction instruction, byte[] operand)
     {
-        if (instruction == Instruction.PRINT)
-        {
-            Console.WriteLine(Encoding.UTF8.GetString([.. currentFrame.Stack.Reverse()]));
-            programCounter++;
-        }
-        else if (instruction == Instruction.PUSH)
-        {
-            foreach (var _byte in operand)
-            {
-                currentFrame.Stack.Push(_byte);
-            }
-            programCounter++;
-        }
+        opcodeActionDictionary[instruction](operand);
     }
 
-    /// <summary>
-    /// Used for 8 bit instructions
-    /// </summary>
-    /// <param name="instruction"></param>
-    /// <param name="operand"></param>
-    /// <exception cref="InvalidOperationException"></exception>
-    private void Execute(Instruction instruction, byte operand)
+    private void ExecutePush(byte[] operand)
     {
-        if (operand == 0)
+        foreach (var value in operand)
         {
-            Console.WriteLine($"{instruction}");
+            currentFrame.Stack.Push(value);
         }
-        else
-        {
-            Console.WriteLine($"{instruction} {operand}");
-        }
-
-        if (instruction == Instruction.POP)
-        {
-            currentFrame.Stack.Pop();
-            programCounter++;
-        }
-        else if (instruction == Instruction.NOP)
-        {
-            programCounter++;
-        }
-        else if (instruction == Instruction.SUM)
-        {
-            if (currentFrame.Stack.Count >= 2)
-            {
-                byte result = 0;
-
-                for (int i = 0; i < currentFrame.Stack.Count; i++)
-                {
-                    var value = currentFrame.Stack.Pop();
-                    result = (byte)(result + value);
-                }
-                currentFrame.Stack.Push(result);
-                Console.WriteLine($"Result: {result}");
-            }
-            programCounter++;
-        }
-        else if (instruction == Instruction.RETURN)
-        {
-            var oldFrameStack = currentFrame.Stack;
-            var parentFrame = currentFrame.Parent;
-            currentFrame = parentFrame ?? mainFrame;
-            _ = currentFrame.Stack.Append(oldFrameStack.Pop());
-            programCounter++;
-        }
-        else if (instruction == Instruction.LOAD)
-        {
-            var bytes = BitConverter.GetBytes(currentFrame.Variables[operand].Value);
-            foreach (var @byte in bytes)
-            {
-                currentFrame.Stack.Push(@byte);
-            }
-            programCounter++;
-        }
-        else
-        {
-            throw new InvalidOperationException($"Instruction {instruction} not exists");
-        }
-    }
-
-    /// <summary>
-    /// Used for 64 bits instructions
-    /// </summary>
-    /// <param name="instruction"></param>
-    /// <param name="operand"></param>
-    /// <exception cref="InvalidOperationException"></exception>
-    private void Execute(Instruction instruction, long operand)
-    {
-
-    }
-
-    /// <summary>
-    /// Used for 32 bits instructions
-    /// </summary>
-    /// <param name="instruction"></param>
-    /// <param name="operand"></param>
-    private void Execute(Instruction instruction, int operand)
-    {
-        switch (instruction)
-        {
-            case Instruction.GOTO:
-                programCounter = operand;
-                break;
-            case Instruction.ISTORE:
-                Console.WriteLine($"Storing at {operand}");
-                if (operand >= currentFrame.Variables.Length)
-                {
-                    var oldVariableArray = currentFrame.Variables;
-                    var size = currentFrame.Variables.Length + 1;
-                    currentFrame.Variables = new Variable[size + 16];
-                    oldVariableArray.CopyTo(currentFrame.Variables, 0);
-                    Console.WriteLine($"> Generating new array and copying data");
-                }
-                var bytes = currentFrame.Stack.Pop(4);
-                currentFrame.Variables[operand] = new Variable(operand, typeof(long), BitConverter.ToInt32(bytes));
-                programCounter++;
-                break;
-            case Instruction.INVOKE:
-                var function = functions[operand];
-                Console.WriteLine($"Invoking {function.Name}");
-                currentFrame = new Frame(function.Name, currentFrame);
-                Run(function.Code);
-                programCounter++;
-                break;
-            default:
-                throw new InvalidOperationException($"Instruction {instruction} not exists");
-
-        }
-
+        programCounter++;
     }
 }
